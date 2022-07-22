ff91740eabc4413f35ecce8c96ed979fe3733c6d
Eloi Stree <eloistree@gmail.com>
2022-07-22 22:33:56 +0200

    Save: 07/22/2022 10:33 PM

---
+++ Runtime/Default/StructNamedBoolean.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructNamedBoolean : INamedBooleanSettable
+{
+    [SerializeField] string m_nameId;
+    [SerializeField] bool m_value;
+    public string GetName()
+    {
+        return m_nameId;
+    }
+    public void GetName(out string name)
+    {
+        name = m_nameId;
+    }
+    public void GetNameRefInContainer(ref string name)
+    {
+        name = m_nameId;
+    }
+    public bool GetValue()
+    {
+        return m_value;
+    }
+    public void GetValue(out bool value)
+    {
+        value = m_value ;
+    }
+    public void GetValueRefInConainer(ref bool value)
+    {
+        value = m_value;
+    }
+    public void SetName(in string name)
+    {
+        m_nameId = name;
+    }
+    public void SetValue(in bool value)
+    {
+        m_value = value;
+    }
+}
---
+++ Runtime/Interface/INamedBoolean.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <summary>
+/// Named boolean is just that:an access to a string and a value that is boolean. Don't even have to be a class can be an index in some arrays.
+/// This is the basic of the project OMI. Because reduce all to boolean allows to have a large API toolbox that don't to change through time.
+/// </summary>
+public interface INamedBoolean: INamedBooleanGet
+{}
+public interface INamedBooleanGet
+{
+    public void GetName(out string name);
+    public void GetValue(out bool value);
+    public void GetNameRefInContainer(ref string name);
+    public void GetValueRefInConainer(ref bool value);
+    public string GetName();
+    public bool GetValue();
+}
+public interface INamedBooleanSettable : INamedBoolean, INamedBooleanSet
+{}
+public interface INamedBooleanSet
+{
+    public void SetName(in string name);
+    public void SetValue(in bool value);
+}
